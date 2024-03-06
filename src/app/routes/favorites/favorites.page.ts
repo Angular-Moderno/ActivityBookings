49430fa6c6b0d9f53855a1b52c01d0aa96e69053
Alberto Basalo <albertobasalo@hotmail.com>
2024-03-06 12:09:18 +0100

    feat: 7 3 3

--- src/app/routes/favorites/favorites.page.ts
+++ src/app/routes/favorites/favorites.page.ts
@@ -11,34 +11,52 @@ import { Observable, forkJoin } from 'rxjs';
   standalone: true,
   imports: [RouterLink],
   template: `
-    @for (activity of activities(); track activity.id) {
-      <span>
-        <a [routerLink]="['/bookings', activity.slug]">{{ activity.name }}</a>
-      </span>
-      <span>at {{ activity.location }} on {{ activity.date }}</span>
+    @for (activity of activities(); track activity) {
+      <div>
+        <span>
+          <a [routerLink]="['/bookings', activity.slug]">{{ activity.name }}</a>
+        </span>
+        <span>at {{ activity.location }} on {{ activity.date }}</span>
+      </div>
       <hr />
     } @empty {
-      <div>No favorites yet</div>
+      <div>No activities yet</div>
     }
   `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class FavoritesPage {
-  #favorites: FavoritesStore = inject(FavoritesStore);
+  // * Injected services division
 
+  // Store for the favorites data
+  #favoritesStore: FavoritesStore = inject(FavoritesStore);
+
+  // Repository to acces activity data on the API
   #activitiesRepository: ActivitiesRepository = inject(ActivitiesRepository);
 
-  #favoriteSlugs: string[] = this.#favorites.state();
+  // * Properties division
 
+  // Load the favorites data from the API
+
+  /** The original signal list of favorites*/
+  // activities: Signal<string[]> = this.#favorites.state;
+
+  /** A simple array with the of slugs of favorite activities*/
+  #favoriteSlugs: string[] = this.#favoritesStore.state();
+
+  /** A function that gets an observable of an activity based on its slug*/
   #getActivityBySlug$ = (favoriteSlug: string) =>
     this.#activitiesRepository.getActivityBySlug$(favoriteSlug);
 
+  /** Maps each element of the array of favorite slugs to an array of activity observable*/
   #mapActivitiesFromSlugs$: Observable<Activity>[] = this.#favoriteSlugs.map(
     this.#getActivityBySlug$,
   );
 
+  /** Forks in parallel, and joins the results in an only one observable of an array of favorite activities*/
   #activities$: Observable<Activity[]> = forkJoin(this.#mapActivitiesFromSlugs$);
 
+  /** The signal list of activities that are favorites to use in the interface*/
   activities: Signal<Activity[]> = toSignal(this.#activities$, { initialValue: [] });
 }
