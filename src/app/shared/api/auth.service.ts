6bd4da7ffcfb33eeaef3b14145b0adbff5ced3f9
Alberto Basalo <albertobasalo@hotmail.com>
2024-04-03 10:29:03 +0200

    feat: pre 8 2 1

--- src/app/shared/api/auth.service.ts
+++ src/app/shared/api/auth.service.ts
@@ -1,28 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Login } from '@domain/login.type';
+import { UserAccessToken } from '@domain/userAccessToken.type';
 import { Observable } from 'rxjs';
 import { Register } from 'src/app/routes/auth/register.form';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthRepositoryService {
+export class AuthRepository {
   #apiUrl = 'http://localhost:3000';
   #http = inject(HttpClient);
 
   postRegister$(register: Register): Observable<UserAccessToken> {
     return this.#http.post<UserAccessToken>(`${this.#apiUrl}/register`, register);
   }
+
+  postLogin$(login: Login): Observable<UserAccessToken> {
+    return this.#http.post<UserAccessToken>(`${this.#apiUrl}/login`, login);
+  }
 }
-
-export type UserAccessToken = {
-  user: User;
-  accessToken: string;
-};
-
-export type User = {
-  id: number;
-  username: string;
-  email: string;
-  terms: boolean;
-};
